a6617a40316d1832bb2be56f5336b1a4d8487579
Samia Ruponti <samia@bongobd.com>
2020-01-03 09:47:05 +0600

    added styles to home page.

--- app/components/Home.js
+++ app/components/Home.js
@@ -3,6 +3,9 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Popup from './common_components/Popup';
 import AddLessonPopup from './lesson_components/AddLessonPopup';
+import studentsIcon from "../img/students.png";
+import lessonIcon from "../img/lessons.png";
+import teachingIcon from "../img/teaching.png";
 
 class HomePage extends React.Component {
   constructor(props) {
@@ -21,45 +24,60 @@ class HomePage extends React.Component {
     // eslint-disable-next-line react/prop-types
     const { user } = this.props;
     return (
-      <div className="col-md-6 col-md-offset-3">
-        {/* eslint-disable-next-line react/prop-types */}
-        <h1>What do you want to do today?</h1>
-        <div className="item">
-          <h2>All Students</h2>
-          <h4>
-            Add a new student, edit existing student details or view progress
-            reports from here.
-          </h4>
+      <div className="container home">
+        <div className="row justify-content-center">
+          <div className="col-md-8">
+            <h1>What do you want to do today?</h1>
+            <div className="row">
+              <div className="col-sm-2">
+                <img src={studentsIcon} alt="all students" />
+              </div>
+              <div className="col-sm-10">
+                <h2>All Students</h2>
+                <h4>
+                  Add a new student, edit existing student details or view
+                  reports from here.
+                </h4>
+              </div>
+            </div>
+            {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-noninteractive-element-interactions */}
+            <div
+              className="row"
+              role="dialog"
+              onClick={this.togglePopup.bind(this)}
+            >
+              {this.state.showPopup ? (
+                <Popup
+                  element={<AddLessonPopup/>}
+                  closePopup={this.togglePopup.bind(this)}
+                />
+              ) : null}
+              <div className="col-sm-2"><img src={lessonIcon} alt="all lessons"/> </div>
+              <div className="col-sm-10">
+                <h2>All Lessons</h2>
+                <h4>
+                  Add new lessons, change previously added lessons, or start a
+                  teaching session.
+                </h4>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-sm-2">
+                <img src={teachingIcon} alt="start teaching" />{' '}
+              </div>
+              <div className="col-sm-10">
+                <h2>Start Teaching</h2>
+                <h4>
+                  Start a new teaching session. You can also start a self study
+                  session, with additional sounds and prompts by the computer.
+                </h4>
+              </div>
+            </div>
+            <p>
+              <Link to="/login">Logout</Link>
+            </p>
+          </div>
         </div>
-        {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-noninteractive-element-interactions */}
-        <div
-          className="item"
-          role="dialog"
-          onClick={this.togglePopup.bind(this)}
-        >
-          {this.state.showPopup ?
-            <Popup
-              element={<AddLessonPopup />}
-              closePopup={this.togglePopup.bind(this)}
-            />
-            : null
-          }
-          <h2>All Lessons</h2>
-          <h4>
-            Add new lessons, change previously added lessons, or start a
-            teaching session.
-          </h4>
-        </div>
-        <div className="item">
-          <h2>Start Teaching</h2>
-          <h4>
-            Start a new teaching session. You can also start a self study
-            session, with additional sounds and prompts by the computer.
-          </h4>
-        </div>
-        <p>
-          <Link to="/login">Logout</Link>
-        </p>
       </div>
     );
   }
